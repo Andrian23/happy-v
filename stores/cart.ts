760fd10170f462b095dfe7c51b94c5b01549f683
justlesik <justlesik@gmail.com>
2024-09-19 17:33:23 +0300

    create cart store

---
+++ stores/cart.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand"
+
+import type { Product } from "@/models/product"
+
+type State = {
+  products: Array<Product & { amount: number }>
+}
+
+type Action = {
+  addProduct: (product: Product, amount: number) => void
+  removeProduct: (productId: number) => void
+}
+
+export const useCartStore = create<State & Action>((set) => ({
+  products: [],
+  addProduct: (product, amount) =>
+    set((state) => {
+      const existingProduct = state.products.find((p) => p.id === product.id)
+      return {
+        products: existingProduct
+          ? state.products.map((p) => (p.id === product.id ? { ...p, amount: p.amount + amount } : p))
+          : [...state.products, { ...product, amount }],
+      }
+    }),
+  removeProduct: (productId) =>
+    set((state) => ({ products: state.products.filter((product) => product.id !== productId) })),
+}))
