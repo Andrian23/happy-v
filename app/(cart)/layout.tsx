1168047a607fc11a308901ef95b22b807c23628c
justlesik <justlesik@gmail.com>
2024-09-19 17:36:12 +0300

    update cart layout

--- app/(cart)/layout.tsx
+++ app/(cart)/layout.tsx
@@ -1,33 +1,14 @@
 import { ReactNode } from "react"
-import Image from "next/image"
-import Link from "next/link"
-import { ArrowLeft } from "lucide-react"
 
-import logo from "@/public/Logo_cl.svg"
-import shieldIcon from "@/public/Shield.svg"
+import { CartHeader } from "@/components/layout/CartHeader"
 
-const CartLayout = ({ children }: { children: ReactNode }) => (
-  <div className="h-full w-full">
-    <div className="flex h-[5rem] w-full items-center justify-between border-b border-grey-400 px-[10rem] py-[2rem] max-lg:px-[1rem]">
-      <Link href="/products">
-        <div className="flex items-center">
-          <ArrowLeft color="#7C8E9E" width={18} height={18} />
-          <div className="ml-2 text-sm text-grey-800">Back</div>
-        </div>
-      </Link>
-      <div className="block max-lg:hidden">
-        <Link href="/dashboard">
-          <Image src={logo} alt="Logo" className="h-10 w-[140px]" />
-        </Link>
-      </div>
-      <div className="hidden text-sm text-grey-800 max-lg:block">Secure Checkout</div>
-      <div className="flex items-center justify-end max-lg:w-[59px]">
-        <Image src={shieldIcon} alt="Secure" className="h-[18px] w-[18px]" />
-        <div className="ml-1 block text-sm text-grey-800 max-lg:hidden">Secure Checkout</div>
-      </div>
+export default function CartLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="h-full">
+      <CartHeader />
+      <main className="grid h-full gap-6 bg-grey-200 px-4 pt-6 lg:grid-cols-5 lg:gap-8 lg:px-44 lg:pt-11">
+        {children}
+      </main>
     </div>
-    {children}
-  </div>
-)
-
-export default CartLayout
+  )
+}
