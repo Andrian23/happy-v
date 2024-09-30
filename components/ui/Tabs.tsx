8172b9beec56cf51de5cff0c71653dce944b8ae7
justlesik <justlesik@gmail.com>
2024-09-30 20:53:24 +0300

    fix tabs layout

--- components/ui/Tabs.tsx
+++ components/ui/Tabs.tsx
@@ -17,12 +17,12 @@ type TabsProps = {
 
 export const Tabs: React.FC<TabsProps> = ({ tabs, activeTab, onChange }) => {
   return (
-    <ul className="m-0 flex gap-1.5 p-0">
+    <ul className="m-0 flex gap-1.5 overflow-x-auto p-0">
       {tabs.map(({ label, value }) => (
         <li
           key={value}
           className={cn(
-            "cursor-pointer bg-grey-200 px-4 py-2 text-sm font-medium text-grey-800 first:rounded-l-xl last:rounded-r-xl",
+            "cursor-pointer whitespace-nowrap bg-grey-200 px-4 py-2 text-sm font-medium text-grey-800 first:rounded-l-xl last:rounded-r-xl",
             { "bg-primary-100 font-semibold text-primary-900": activeTab === value }
           )}
           onClick={() => onChange(value)}
