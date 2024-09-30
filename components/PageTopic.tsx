ad094749ad41e549d10f94a23d9433315bdd2364
justlesik <justlesik@gmail.com>
2024-09-30 22:04:55 +0300

    fix page topic

--- components/PageTopic.tsx
+++ components/PageTopic.tsx
@@ -26,7 +26,7 @@ const PageTopic: React.FC<PageTopicProps> = ({ name, description, children, stic
         {children}
         <Link href="/cart" className="relative ml-auto max-lg:hidden">
           <Cart className="h-6 w-6 text-primary-900" />
-          <Badge className="absolute -right-1 -top-1 flex aspect-square justify-center rounded-full p-0.5 text-[10px] leading-none">
+          <Badge className="absolute -right-1 -top-1 flex aspect-square min-w-4 items-center justify-center rounded-full p-0.5 text-[10px] leading-none">
             {count}
           </Badge>
         </Link>
