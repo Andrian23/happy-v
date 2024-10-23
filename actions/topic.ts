78209ba4466c8337f96a182c193c0ca9299e52d6
Oles Leskiv <olesleskiv@Oless-MacBook-Pro.local>
2024-10-23 18:06:50 +0300

    extend createTopic response with author data

--- actions/topic.ts
+++ actions/topic.ts
@@ -7,7 +7,18 @@ import { TopicData, topicSchema } from "@/schemas/topic"
 
 import { db } from "../lib/db"
 
-export async function createTopic(topicData: TopicData): Promise<Topic> {
+const topicWithAuthorAndRepliesCountQuery = {
+  include: {
+    author: {
+      select: { name: true, lastName: true, type_proffesion: true, image: true },
+    },
+    _count: { select: { replies: true } },
+  },
+}
+
+export type TopicWithAuthor = Prisma.TopicGetPayload<typeof topicWithAuthorAndRepliesCountQuery>
+
+export async function createTopic(topicData: TopicData): Promise<TopicWithAuthor> {
   const session = await auth()
   const userId = session?.user?.id
 
@@ -22,22 +33,11 @@ export async function createTopic(topicData: TopicData): Promise<Topic> {
   }
 
   try {
-    return db.topic.create({ data: { ...topicData, authorId: userId } })
+    return db.topic.create({ data: { ...topicData, authorId: userId }, ...topicWithAuthorAndRepliesCountQuery })
   } catch (error) {
     throw new Error((error as unknown as Error).message)
   }
 }
-
-const topicWithAuthorAndRepliesCountQuery = {
-  include: {
-    author: {
-      select: { name: true, lastName: true, type_proffesion: true, image: true },
-    },
-    _count: { select: { replies: true } },
-  },
-}
-
-export type TopicWithAuthor = Prisma.TopicGetPayload<typeof topicWithAuthorAndRepliesCountQuery>
 
 export async function getTopics(params?: Partial<Topic>): Promise<{ topics: TopicWithAuthor[]; count: number }> {
   try {
@@ -62,7 +62,13 @@ const topicWithAuthorAndReplies = {
       select: { name: true, lastName: true, type_proffesion: true, image: true },
     },
     _count: { select: { replies: true } },
-    replies: true,
+    replies: {
+      include: {
+        author: {
+          select: { name: true, lastName: true, type_proffesion: true, image: true },
+        },
+      },
+    },
   },
 }
 export type TopicWithAuthorAndReplies = Prisma.TopicGetPayload<typeof topicWithAuthorAndReplies>
@@ -72,15 +78,5 @@ export async function getTopicById(id: string): Promise<TopicWithAuthorAndReplie
     return db.topic.findUnique({ where: { id }, ...topicWithAuthorAndReplies })
   } catch (error) {
     throw new Error((error as unknown as Error).message)
-  }
-}
-
-export async function countTopics(): Promise<number> {
-  try {
-    const count = await db.topic.count()
-    return count
-  } catch (error) {
-    console.error("Failed to count topics:", error)
-    throw error
   }
 }
