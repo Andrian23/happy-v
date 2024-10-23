bc955d1606abdba0560ef981a9fad1aab407566c
Oles Leskiv <olesleskiv@Oless-MacBook-Pro.local>
2024-10-23 18:07:48 +0300

    add createReply server action

---
+++ actions/reply.ts
@@ -0,0 +1,42 @@
+"use server"
+
+import { Prisma } from "@prisma/client"
+
+import { auth } from "@/auth"
+import { ReplyData, replySchema } from "@/schemas/reply"
+
+import { db } from "../lib/db"
+
+const replyWithAuthor = {
+  include: {
+    author: {
+      select: { name: true, lastName: true, type_proffesion: true, image: true },
+    },
+  },
+}
+
+export type ReplyWithAuthor = Prisma.ReplyGetPayload<typeof replyWithAuthor>
+
+export async function createReply(topicId: string, replyData: ReplyData): Promise<ReplyWithAuthor> {
+  const session = await auth()
+  const userId = session?.user?.id
+
+  if (!userId) {
+    throw new Error("User not authenticated")
+  }
+
+  const validation = replySchema.safeParse(replyData)
+
+  if (!validation.success) {
+    throw new Error(validation.error.errors[0].message)
+  }
+
+  try {
+    return db.reply.create({
+      data: { ...replyData, authorId: userId, topicId },
+      ...replyWithAuthor,
+    })
+  } catch (error) {
+    throw new Error((error as unknown as Error).message)
+  }
+}
