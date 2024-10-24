d98a2f15ff52bb516ad5fa30f9659cae0dd223a2
Oles Leskiv <olesleskiv@Oless-MacBook-Pro.local>
2024-10-24 13:26:27 +0300

    update TopicContent component with likes related props

--- components/community/TopicContent.tsx
+++ components/community/TopicContent.tsx
@@ -1,17 +1,26 @@
 import React from "react"
 import { Heart } from "lucide-react"
 
+import { cn } from "@/lib/utils"
+
 type ContentProps = {
   content: string
+  onLike: () => void
+  likes: number
+  isLiked: boolean
 }
 
-export const TopicContent: React.FC<ContentProps> = ({ content }) => (
-  <div className="ml-5 mt-2.5 border-l-2 border-grey-400 pl-7 text-primary-900">
-    <div dangerouslySetInnerHTML={{ __html: content }} />
-    <div className="mt-3 flex justify-end gap-1">
-      {/* TODO: add like handler and counter */}
-      <Heart onClick={() => {}} className="cursor-pointer text-primary-500" />
-      <span className="text-primary-800">0</span>
+export const TopicContent: React.FC<ContentProps> = ({ content, onLike, likes, isLiked }) => {
+  return (
+    <div className="ml-5 mt-2.5 border-l-2 border-grey-400 pl-7 text-primary-900">
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+      <div className="mt-3 flex items-center justify-end gap-1">
+        <Heart
+          onClick={onLike}
+          className={cn("h-5 w-5 cursor-pointer text-primary-500", isLiked && "fill-primary-500")}
+        />
+        <span className="text-sm font-semibold text-primary-800">{likes}</span>
+      </div>
     </div>
-  </div>
-)
+  )
+}
