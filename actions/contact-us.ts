069c8491fef39f33b3009d434340a17adcbc0c07
Vitalii Tarasiuk <91137464+VitaliiPro@users.noreply.github.com>
2024-11-01 17:08:20 +0200

    Refactored

--- actions/contact-us.ts
+++ actions/contact-us.ts
@@ -26,7 +26,6 @@ export const createTicketAction = async ({
         Authorization: `Basic ${Buffer.from(`${process.env.NEXT_PUBLIC_GORGIAS_EMAIL}:${process.env.NEXT_PUBLIC_GORGIAS_API_KEY}`).toString("base64")}`,
       },
       body: JSON.stringify({
-        status: "open",
         customer: {
           email,
         },
