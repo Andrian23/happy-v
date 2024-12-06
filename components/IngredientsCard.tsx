1c46be75148e867b3204702e4ca46531531cdfaa
Vitalii Tarasiuk <91137464+VitaliiPro@users.noreply.github.com>
2024-12-06 18:01:11 +0200

    Hot fixes

--- components/IngredientsCard.tsx
+++ components/IngredientsCard.tsx
@@ -21,29 +21,35 @@ export const IngredientsCard: React.FC<IngredientsCardProps> = ({
 }) => {
   return (
     <div className={cn("grid gap-5 rounded-2xl bg-grey-200 p-4 text-primary-900 lg:gap-6 lg:p-6", className)}>
-      <div className="grid gap-1.5">
+      <div className="flex flex-col gap-1.5">
         <h5 className="text-base font-bold">Ingredient Amounts</h5>
         {servingSize && <div className="text-sm">{`Serving Size: ${servingSize}`}</div>}
         {servingsNumber && <div className="text-sm">{`Servings Per Container: ${servingsNumber}`}</div>}
       </div>
 
-      <div className="grid gap-1.5">
-        <div className="text-base font-semibold">How to Use</div>
-        <div className="text-sm">{howToUse || "Usage instructions will be available soon."}</div>
-      </div>
-
-      <div className="grid gap-2">
-        <h5 className="text-base font-bold">Warnings</h5>
-        <div>
-          <h6 className="text-sm font-bold">Allergy Warning</h6>
-          <div className="text-sm">{allergyWarning || "Safety information will be available soon."}</div>
+      {howToUse && (
+        <div className="flex flex-col gap-1.5">
+          <div className="text-base font-semibold">How to Use</div>
+          <div className="text-sm">{howToUse}</div>
         </div>
-      </div>
+      )}
 
-      <div className="grid gap-1.5">
-        <h5 className="text-base font-bold">Storage</h5>
-        <div className="text-sm">{storage || "Storage instructions will be available soon."}</div>
-      </div>
+      {allergyWarning && (
+        <div className="flex flex-col gap-2">
+          <h5 className="text-base font-bold">Warnings</h5>
+          <div>
+            <h6 className="text-sm font-bold">Allergy Warning</h6>
+            <div className="text-sm">{allergyWarning}</div>
+          </div>
+        </div>
+      )}
+
+      {storage && (
+        <div className="flex flex-col gap-1.5">
+          <h5 className="text-base font-bold">Storage</h5>
+          <div className="text-sm">{storage}</div>
+        </div>
+      )}
     </div>
   )
 }
